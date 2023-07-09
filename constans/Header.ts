cc008478229cdc9916be276e88761eb3659179e8
Serenbek Ibraimkulov <serenbek@mail.ru>
2023-07-09 11:27:57 +0600

    addedImg

--- constans/Header.ts
+++ constans/Header.ts
@@ -2,4 +2,5 @@ export const headerLinks = [
   { label: "Home", href: "/" },
   { label: "Blog", href: "/blog" },
   { label: "About", href: "/about" },
+  { label: "Pictures", href: "/pictures" },
 ];
